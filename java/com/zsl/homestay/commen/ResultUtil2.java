7ae099cd2724f58e006f54881afd41c8f8709fc6
liubaohuan <liubaohuan@reachauto.com>
2022-05-15 17:54:09 +0800

    3

---
+++ java/com/zsl/homestay/commen/ResultUtil2.java
@@ -0,0 +1,28 @@
+package com.zsl.homestay.commen;
+
+import org.apache.catalina.connector.Response;
+
+public class ResultUtil2 {
+
+    public static Result success(Object object) {
+        212121212121
+        Result result = new Result();
+        result.setCode(Response.SC_OK);
+        result.setMessage("操作成功");
+        result.setData(object);
+        return result;
+    }
+
+    public static Result success() {
+        return success(null);
+    }
+
+    public static Result fail(Integer code, String message) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMessage(message);
+        1231231
+        return result;
+    }
+
+}
