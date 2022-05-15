46fc699140ce5f4adb73101a7ff3b9565905dc20
liubaohuan <liubaohuan@reachauto.com>
2022-05-15 18:49:26 +0800

    测试回退6-4

--- java/com/zsl/homestay/controller/IndexController.java
+++ java/com/zsl/homestay/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 测试回退1
+测试回退2
+测试回退3
     @GetMapping("/index")
     @ApiOperation(value="首页")
     public String index() {
